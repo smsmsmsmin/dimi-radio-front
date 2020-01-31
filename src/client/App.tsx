0c3ee258dc8a43d1dd3665aba82732cfac413a94
Lee Seungmin <me@2sm.kr>
2020-01-31 18:58:21 +0900

    apollo client

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "@apollo/react-hooks";
+import configureClient from "./configureApolloClient";
 import { Root } from "../pages";
+
+const client = configureClient();
+
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Root} />
-    </Switch>
+    <ApolloProvider client={client}>
+      <ApolloHooksProvider client={client}>
+        <Switch>
+          <Route exact path="/" component={Root} />
+        </Switch>
+      </ApolloHooksProvider>
+    </ApolloProvider>
   </BrowserRouter>
 );
 
 export default App;
+
---
+++ src/client/configureApolloClient.ts
@@ -0,0 +1,21 @@
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache
+} from "apollo-boost";
+
+export default function configureClient() {
+  const httpLink = new HttpLink({
+    uri: "http://localhost:4000"
+  });
+
+  const authLink = new ApolloLink((operation, forward) => {
+    return forward(operation);
+  });
+
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  });
+}
